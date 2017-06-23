3d508b04f574a8b2f106778c51b4da2daa6f73f0
mrexodia <mr.exodia.tpodt@gmail.com>
2017-06-23 12:49:32 +0200

    update

--- sustainable-info-society.js
+++ sustainable-info-society.js
@@ -1,1 +1,1 @@
-$('head').append('<link rel="stylesheet" type="text/css" href="//github.com/mrexodia/sustainable-info-society/raw/master/sustainable-info-society.css">');
+$('head').append('<link rel="stylesheet" type="text/css" href="//rawgit.com/mrexodia/sustainable-info-society/master/sustainable-info-society.css">');
