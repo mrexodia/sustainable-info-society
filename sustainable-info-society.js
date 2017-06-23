bb04325e0589a54aba2da2c3d558463aef937b03
mrexodia <mr.exodia.tpodt@gmail.com>
2017-06-23 13:29:20 +0200

    no cache for css

--- sustainable-info-society.js
+++ sustainable-info-society.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
-    $('head').append('<link rel="stylesheet" type="text/css" href="//raw.githack.com/mrexodia/sustainable-info-society/master/sustainable-info-society.css">');
+    //$('head').append('<link rel="stylesheet" type="text/css" href="//raw.githack.com/mrexodia/sustainable-info-society/master/sustainable-info-society.css">');
+    $('head').append('<link rel="stylesheet" type="text/css" href="//github.com/mrexodia/sustainable-info-society/raw/master/sustainable-info-society.css">');
 });
