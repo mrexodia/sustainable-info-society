f950caf462202a5eeaa8208f1f218c60663dc6f3
mrexodia <mr.exodia.tpodt@gmail.com>
2017-06-23 13:30:27 +0200

    grrr

--- sustainable-info-society.js
+++ sustainable-info-society.js
@@ -1,4 +1,3 @@
 $(document).ready(function() {
-    //$('head').append('<link rel="stylesheet" type="text/css" href="//raw.githack.com/mrexodia/sustainable-info-society/master/sustainable-info-society.css">');
-    $('head').append('<link rel="stylesheet" type="text/css" href="//github.com/mrexodia/sustainable-info-society/raw/master/sustainable-info-society.css">');
+    $('head').append('<link rel="stylesheet" type="text/css" href="//raw.githack.com/mrexodia/sustainable-info-society/master/sustainable-info-society.css">');
 });
