fa38c7a04f78bcc91819dbfb329883ade97e9df2
mrexodia <mr.exodia.tpodt@gmail.com>
2017-06-23 12:51:58 +0200

    on ready

--- sustainable-info-society.js
+++ sustainable-info-society.js
@@ -1,1 +1,3 @@
-$('head').append('<link rel="stylesheet" type="text/css" href="//rawgit.com/mrexodia/sustainable-info-society/master/sustainable-info-society.css">');
+$(document).ready(function() {
+    $('head').append('<link rel="stylesheet" type="text/css" href="//rawgit.com/mrexodia/sustainable-info-society/master/sustainable-info-society.css">');
+});
